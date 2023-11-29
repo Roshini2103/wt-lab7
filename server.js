2633e2d26da7a23f21d34b3b15bd2f2c04f6a5ea
abhijithkrishnar <114537538+abhijithkrishnar@users.noreply.github.com>
2023-11-28 17:31:48 -0700

    first commit

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const logger = require('morgan')
-const path = require('path')
-const server = express()
-server.use(express.urlencoded({'extended': true}))
-server.use(logger('dev'))
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
+const server = express();
+server.use(express.urlencoded({ extended: true }));
+server.use(logger("dev"));
 
 // Routes
-server.get('/do_a_random', (req, res) => {
-  res.send(`Your number is: ${Math.floor(Math.random() * 100) + 1}`)
-})
+server.get("/do_a_random", (req, res) => {
+  res.send(`Your number is: ${Math.floor(Math.random() * 100) + 1}`);
+});
 
-server.post('/itc505/lab7', (req, res) => {
+server.post("/itc505/lab7", (req, res) => {
   const noun = req.body.noun;
   const noun1 = req.body.noun1;
   const noun2 = req.body.noun2;
@@ -19,7 +19,7 @@ server.post('/itc505/lab7', (req, res) => {
   const verb = req.body.verb;
 
   const madLib = `As ${noun} walked through the ${adjective1} ${noun1}, they couldn't shake the feeling of being watched. Every creak of the floorboard, every rustle of the leaves, sent a chill down their spine.Suddenly, they heard a sound that made their heart leap into their throat. It was a soft, scratching noise, like something was being dragged across the floor. ${noun} froze in their tracks, their eyes darting around in the dim light.Then, they saw it - a ${adjective2}  emerging from the shadows. It was tall and gaunt, with sunken eyes and a long, claw-like hand reaching out towards ${noun}.${noun} let out a scream and turned to run, but their foot caught on something and they fell to the ground.`;
-  
+
   res.send(`
     madLib<!DOCTYPE html>
     <html lang="en">
@@ -43,21 +43,19 @@ server.post('/itc505/lab7', (req, res) => {
     </body>
     </html>
     `);
-
 });
 
 // Setup static page serving for all the pages in "public"
 
-const publicServedFilesPath = path.join(__dirname, 'public')
-server.use(express.static(publicServedFilesPath))
-
+const publicServedFilesPath = path.join(__dirname, "public");
+server.use(express.static(publicServedFilesPath));
 
 // The server uses port 80 by default unless you start it with the extra
 // command line argument 'local' like this:
 //       node server.js local
-let port = 80
-if (process.argv[2] === 'local') {
-  port = 8081
+let port = 8081;
+if (process.argv[2] === "local") {
+  port = 8081;
 }
 
-server.listen(port, () => console.log('Ready on localhost!'))
+server.listen(port, () => console.log("Ready on localhost!"));
